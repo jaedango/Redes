abf957413034785200840f3037142814bd942899
jaedango <javier.andrews@ug.uchile.cl>
2021-05-25 18:39:56 -0400

    Se modifica receptor.py, a un formato mas aceptable, falta revisar y modificar emisor.py

--- 04TCPUDP/receptor.py
+++ 04TCPUDP/receptor.py
@@ -1,58 +1,82 @@
 # -*- coding: utf-8 -*-
+from os import access
 import socket
 
+# ---------------------------------------------
+def toDecode(message):
+    msg = message.decode()
+    list = msg.split(".")
+    return list[0], list[1], list[2]
+
+# ---------------------------------------------
+def toEncode(ACK, SYN, msg):
+    ACK1 = checkOne(ACK)
+    resp = "{}.{}.{}".format(ACK1, SYN, msg)
+    return resp.encode()
+
+# ---------------------------------------------
+def checkOne(msg):
+    try:
+        msg1 = int(msg) + 1
+    except:
+        msg1 = ""
+    return msg1
+
+# ---------------------------------------------
+def tripleHandShakeServer():
+    payload, client_address = server_socket.recvfrom(buffsize)
+
+    # ---------------------------------------------
+    # ## Client Message
+    ACK, SYN, msg = toDecode(payload)
+    print("Mensaje recibido : <<{}>>", format(msg))
+
+    # ---------------------------------------------
+    # ## Server Response
+    SYN = "200"
+    Server_Resp = toEncode(ACK, SYN, msg)
+    sent = server_socket.sendto(Server_Resp, client_address)
+
+    # ---------------------------------------------
+    # ## Client Response
+    while True:
+        server_socket.settimeout(timeout)
+        try:
+            payload, client_address = server_socket.recvfrom(buffsize)
+            ACK, SYN1, msg = toDecode(payload)
+            if SYN1 == checkOne(SYN):
+                print("Coneccion establecida correctamente.")
+                break
+            else:
+                print("Ocurrio un transmitiendo el mensaje")
+                break
+
+        except socket.timeout:
+            print("No se recibio mensaje alguno")
+            server_socket.settimeout(None)
+            break
+
+    
+
+
+
+# ---------------------------------------------
+# ---------------------------------------------
+# ## direccion
 server_address = 'localhost'
 server_port = 5000
 buffsize = 64
+timeout = 1
 
 address = (server_address, server_port)
 
-print('Creando socket - Servidor')
+# ---------------------------------------------
+# Creando servidor
+print('Crando Socket - Servidor')
 # Socket no orientado a conexion
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(address)
-
-print("Listening on " + server_address + " : " + str(server_port))
-
+listening = "Listening on {} : {}".format(server_address, server_port)
+print(listening)
 while True:
-    # recibir mensajes. Este metodo nos entrega el mensaje
-    # junto a la direccion de origen del mensaje
-    payload, client_address = server_socket.recvfrom(buffsize)
-
-    # ---------------------------------------------
-    ## client mensaje 1
-    # imprimimos el mensaje recibido
-    mensaje1 = payload.decode()
-    respsms = mensaje1[:3]
-    try:
-        respsms = int(respsms) + 1
-        respsms = str(respsms)
-    except:
-        print("error\n")
-    
-    print("Mensaje1 = <<" + mensaje1[6:] + ">>")
-
-    # ---------------------------------------------
-    ## server response 1
-    print("Echoing data back to " + str(client_address))
-    sms2 = "200"
-    response = respsms + sms2
-    sent = server_socket.sendto(response.encode(), client_address)
-    
-    # ---------------------------------------------
-    ## client mensaje 2
-    payload, client_address = server_socket.recvfrom(buffsize)
-    mensaje2 = payload.decode()
-    ack = respsms[:3]
-    print(ack + "-> ack\n")
-    try:
-        respsms2 = mensaje1[3:6]
-        respsms2 = int(respsms) + 1
-        respsms2 = str(respsms2)
-    except:
-        print("error\n")
-    print("Mensaje2 = <<" + mensaje2 + ">>")
-    print("Coneccion establecida correctamente")
-    # ---------------------------------------------
-    ## server response 2
-    sent = server_socket.sendto(respsms2.encode(),client_address)
+    tripleHandShakeServer()
